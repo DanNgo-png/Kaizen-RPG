16fccb203dceea519dcf79011c1d5a67ff54f39c
Dan-Ngo-png <dngo2107@gmail.com>
2025-11-30 03:45:02 -0600

    Fixed configure button issue

--- resources/main.js
+++ resources/main.js
@@ -1,6 +1,7 @@
 import { handleDropdowns } from './js/dropdown.js'
 import { toggleSideBar } from './js/toggleSideBar.js'
 import { loadPage } from './js/router.js'
+import { configureSidebar } from './js/focus/configureSidebar.js'
 
 function app() {
     handleDropdowns()
@@ -15,12 +16,23 @@ function app() {
     const focusButton = document.querySelector(".focus-button");
     if (focusButton) {
         focusButton.addEventListener("click", () => {
-            loadPage('./pages/focus.html');
+            loadPage('./pages/focus-standard.html');
         });
     }
 
     // 3. Setup "Plan" or "Home" Buttons (Optional)
     // If you have a button to go back home, add a listener for './pages/home.html'
+
+    // Event delegation for dynamically loaded content
+    document.addEventListener('click', function(event) {
+        // Configure Sidebar Button
+        if (event.target.closest('#configure-footer-button')) {
+            configureSidebar();
+            return; // Stop further checks
+        }
+
+        // NOTE: You can add more delegated events here in the future
+    });
 }
 
 app()
