764adcc5bb947b171f69e0404fef92fa8f1caa1f
Dan-Ngo-png <dngo2107@gmail.com>
2025-12-24 08:32:27 -0600

    improved run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,2 @@
-cd Kaizen-RPG
-if not errorlevel 1 (
-    neu run
-) else (
-    echo The 'cd Kaizen-RPG' command failed. Trying 'neu run' in the current directory.
-    neu run
-)
+cd /d "%~dp0"
+neu run
