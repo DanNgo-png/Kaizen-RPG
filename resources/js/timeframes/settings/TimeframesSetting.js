326dd36aa5658103e5e17f7a6d395e6bd8a28cae
Dan-Ngo-png <dngo2107@gmail.com>
2026-01-09 10:59:36 -0600

    filled in timeframesettings.js

---
+++ resources/js/timeframes/settings/TimeframesSetting.js
@@ -0,0 +1,57 @@
+import { SettingsAPI } from "../../api/SettingsAPI.js";
+
+export class TimeframesSetting {
+    constructor() {
+        this.dom = {
+            startHour: document.getElementById('tf-start-hour'),
+            endHour: document.getElementById('tf-end-hour'),
+            showWeekends: document.getElementById('tf-show-weekends')
+        };
+
+        this.init();
+    }
+
+    init() {
+        if (!this.dom.startHour) {
+            console.warn("TimeframesSetting: DOM elements not found. Are you on the correct page?");
+            return;
+        }
+
+        // 1. Bind Listeners (UI -> DB)
+        this.dom.startHour.addEventListener('change', (e) => {
+            SettingsAPI.saveSetting('timelineStartHour', e.target.value);
+        });
+
+        this.dom.endHour.addEventListener('change', (e) => {
+            SettingsAPI.saveSetting('timelineEndHour', e.target.value);
+        });
+
+        if (this.dom.showWeekends) {
+            this.dom.showWeekends.addEventListener('change', (e) => {
+                SettingsAPI.saveSetting('timelineShowWeekends', e.target.checked);
+            });
+        }
+
+        // 2. Listen for Data (DB -> UI)
+        // We define the handler so we can remove it later if needed (though page navigates away usually)
+        const updateHandler = (e) => {
+            const { key, value } = e.detail;
+            if (key === 'timelineStartHour' && this.dom.startHour) this.dom.startHour.value = value;
+            if (key === 'timelineEndHour' && this.dom.endHour) this.dom.endHour.value = value;
+            if (key === 'timelineShowWeekends' && this.dom.showWeekends) {
+                this.dom.showWeekends.checked = (value === 'true' || value === true);
+            }
+        };
+
+        document.addEventListener('kaizen:setting-update', updateHandler);
+
+        // 3. Load Initial Values
+        SettingsAPI.getSetting('timelineStartHour');
+        SettingsAPI.getSetting('timelineEndHour');
+        SettingsAPI.getSetting('timelineShowWeekends');
+    }
+}
+
+export function initTimeframeSettings() {
+    new TimeframesSetting();
+}
