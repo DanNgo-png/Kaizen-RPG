87c2c11e2665b7c4dfa08f69a170cb180fdac5a9
Dan-Ngo-png <dngo2107@gmail.com>
2026-01-09 10:59:24 -0600

    imported SettingsAPI

--- resources/js/timeframes/TodayManager.js
+++ resources/js/timeframes/TodayManager.js
@@ -1,4 +1,5 @@
 import { EXTENSION_ID } from "../api/_extension_id.js";
+import { SettingsAPI } from "../api/SettingsAPI.js";
 import { TaskPoolManager } from "./today/TaskPoolManager.js";
 import { TimelineManager } from "./today/TimelineManager.js";
 import { TodayUIManager } from "./today/TodayUIManager.js";
@@ -8,23 +9,49 @@ export class TodayManager {
         this.now = new Date();
         this.dateKey = this.now.toISOString().split('T')[0];
 
-        // Initialize Sub-Managers
         this.uiManager = new TodayUIManager(this.dateKey, this.now);
         this.poolManager = new TaskPoolManager(this.dateKey);
         this.timelineManager = new TimelineManager(this.dateKey);
+
+        // State for settings
+        this.timelineSettings = { start: 9, end: 17 };
 
         this.init();
     }
 
     init() {
-        // Bind Backend Events
+        // 1. Data Listeners
         Neutralino.events.off('receiveTodayData', this.handleData);
         Neutralino.events.on('receiveTodayData', (e) => this.handleData(e));
 
         Neutralino.events.off('refreshTodayView', this.handleRefresh);
         Neutralino.events.on('refreshTodayView', (e) => this.handleRefresh(e));
 
+        // 2. Settings Listeners
+        document.addEventListener('kaizen:setting-update', (e) => {
+            const { key, value } = e.detail;
+            if (key === 'timelineStartHour') {
+                this.timelineSettings.start = value;
+                this.timelineManager.updateConfig(this.timelineSettings.start, this.timelineSettings.end);
+            }
+            if (key === 'timelineEndHour') {
+                this.timelineSettings.end = value;
+                this.timelineManager.updateConfig(this.timelineSettings.start, this.timelineSettings.end);
+            }
+        });
+
+        // 3. Initial Load Sequence
+        this.loadSettings();
         this.fetchData();
+    }
+
+    loadSettings() {
+        // Request settings (async)
+        SettingsAPI.getSetting('timelineStartHour');
+        SettingsAPI.getSetting('timelineEndHour');
+        
+        // Note: Timeline will update when the events fire (usually ms later).
+        // Default 9-17 is already set in TimelineManager constructor.
     }
 
     fetchData() {
@@ -33,10 +60,8 @@ export class TodayManager {
 
     handleData(e) {
         const data = e.detail;
-        
-        // Delegate to sub-managers
         this.uiManager.updateDailyGoal(data.dailyGoal);
-        this.poolManager.render(data.pool, data.schedule); // Schedule needed for smart stacking calculation
+        this.poolManager.render(data.pool, data.schedule);
         this.timelineManager.render(data.schedule);
     }
 
