b8ecd6d8c3121f75ac1d2fc80df6d1a593af090c
Dan-Ngo-png <dngo2107@gmail.com>
2026-01-16 17:08:16 -0600

    improve .bat file to hide python terminal

--- run_2.bat
+++ run_2.bat
@@ -5,19 +5,13 @@ set "dan_laptop=C:\Users\danng\OneDrive\Desktop\Projects\Code-Cartographer"
 set "dan_computer=C:\Users\minhn\Desktop\Projects\Code-Cartographer"
 
 if exist "%dan_laptop%\main.py" (
-    echo "Starting Code Cartographer from Laptop path..."
-    start /d "%dan_laptop%" python main.py
+    start "" /d "%dan_laptop%" pythonw main.py
 ) else if exist "%dan_computer%\main.py" (
-    echo "Starting Code Cartographer from Desktop path..."
-    start /d "%dan_computer%" python main.py
-) else (
-    echo "Warning: Could not find App #1"
+    start "" /d "%dan_computer%" pythonw main.py
 )
 
 :: --- APP #2: Kaizen-RPG ---
-echo "Starting App #2..."
 cd /d "%~dp0"
-start "" neu run
+start /b "" neu run
 
-echo Both applications are launching.
-pause
+exit
