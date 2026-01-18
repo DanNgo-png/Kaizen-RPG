e78b98c096b69cfd4631e4604522ae5defea10a1
Dan-Ngo-png <dngo2107@gmail.com>
2026-01-18 13:35:29 -0600

    removed party, quests, inventory buttons

--- resources/js/_main-app/load_page/LoadGame.js
+++ resources/js/_main-app/load_page/LoadGame.js
@@ -9,29 +9,6 @@ export function setupGameNavigation() {
             initMenuButtons();
         });
     }
-
-    const managementButton = document.querySelector(".game-party-button");
-    if (managementButton) {
-        managementButton.addEventListener("click", async () => {
-            await loadPage('./pages/games/management.html'); 
-            initManagement();
-        });
-    }
-
-    const questButton = document.querySelector(".game-quests-button");
-    if (questButton) {
-        questButton.addEventListener("click", async () => {
-            await loadPage('./pages/games/quests.html');
-        });
-    }
-
-    const inventoryButton = document.querySelector(".game-inventory-button");
-    if (inventoryButton) {
-        inventoryButton.addEventListener("click", async () => {
-            await loadPage('./pages/games/management.html');
-            initManagement();
-        });
-    }
     
     const gameSettingsButton = document.querySelector(".game-settings-button");
     if (gameSettingsButton) {
