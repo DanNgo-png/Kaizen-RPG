11791caffb98682f28cfc256e6f1958f872194c0
Dan-Ngo-png <dngo2107@gmail.com>
2026-05-14 20:13:49 -0500

    feat: Add faction display logic and color handling for selected nodes

--- resources/js/games/world/ui/barebones/BarebonesTemplates.js
+++ resources/js/games/world/ui/barebones/BarebonesTemplates.js
@@ -1,3 +1,6 @@
+const DEFAULT_FACTION_COLOR = "#60a5fa";
+const HEX_COLOR_PATTERN = /^#[0-9a-f]{6}$/i;
+
 export function escapeHtml(value) {
     return String(value ?? "")
         .replaceAll("&", "&amp;")
@@ -19,10 +22,28 @@ export function loadingStateHtml(message) {
 export function selectedNodeLabelHtml(node) {
     if (!node) return "";
 
+    const factionHtml = node.faction
+        ? selectedFactionHtml(node.faction)
+        : "";
+
     return `
         &mdash; ${escapeHtml(node.name)}
         <span class="bb-selected-reputation">
             <i class="fa-solid fa-handshake"></i> Rep: ${Number(node.reputation) || 0}
+        </span>
+        ${factionHtml}
+    `;
+}
+
+function selectedFactionHtml(faction) {
+    const color = HEX_COLOR_PATTERN.test(String(faction.color ?? ""))
+        ? faction.color
+        : DEFAULT_FACTION_COLOR;
+
+    return `
+        <span class="bb-selected-faction" style="--faction-color:${color};">
+            <span class="bb-selected-faction-dot"></span>
+            ${escapeHtml(faction.name)}
         </span>
     `;
 }
